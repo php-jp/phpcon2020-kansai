55d895f18a57cfd9dee6f61d61dfeb15011c2c31
ririko <ririmohu25@gmail.com>
2020-02-19 14:43:29 +0900

    text修正

--- pages/index.vue
+++ pages/index.vue
@@ -26,14 +26,14 @@ export default {
     Share
   },
   head: {
-      title: 'PHPカンファレンス関西2020 は 5月9日(土)にグランフロント大阪にて開催!',
+      title: 'PHPカンファレンス関西2020 は 5月9日(土)に開催!',
       meta: [
-        { property: 'og:title', content: 'PHPカンファレンス関西2020 は 5月9日(土)にグランフロント大阪にて開催!' },
+        { property: 'og:title', content: 'PHPカンファレンス関西2020 は 5月9日(土)に開催!' },
         { property: 'og:image', content: 'https://2020.kphpug.jp/images/ogp.jpg' },
         { property: 'og:url', content: 'https://2020.kphpug.jp/' },
-        { property: 'og:description', content: '関西PHPerの夏はこれがなくちゃ始まらない！今年で9回目となる関西最大級のPHPイベント、今年も開催です。全国PHPerが大阪に集結「PHPカンファレンス関西」で心に残る夏の思い出を！' },
+        { property: 'og:description', content: '集え、PHPer！！PHPerのGWはこの日が本番あのPHPの祭典がついに帰ってきた！今回のテーマは「Share!!」当日はPHPに関するトークセッションのほか、懇親会やその他イベントを実施予定「PHPカンファレンス関西」で知識•技術•感動をみんなで分かち合おう！' },
         { name: 'twitter:card', content: 'summary_large_image' },
-        { name: 'description', content: '関西PHPerの夏はこれがなくちゃ始まらない！今年で9回目となる関西最大級のPHPイベント、今年も開催です。全国PHPerが大阪に集結「PHPカンファレンス関西」で心に残る夏の思い出を！' }
+        { name: 'description', content: '集え、PHPer！！PHPerのGWはこの日が本番あのPHPの祭典がついに帰ってきた！今回のテーマは「Share!!」当日はPHPに関するトークセッションのほか、懇親会やその他イベントを実施予定「PHPカンファレンス関西」で知識•技術•感動をみんなで分かち合おう！' }
 
       ]
     }
