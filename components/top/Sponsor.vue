5222864fa3dcd4c7ee6e951013169c41f7a19311
中村駿輝 <shnklife@gmail.com>
2020-03-24 13:56:21 +0900

    Sponsorコンポーネント新規作成、読み込み

---
+++ components/top/Sponsor.vue
@@ -0,0 +1,28 @@
+<template>
+    <section class="p-sponsor">
+        <div class="l-container">
+            <div class="p-sponsor__title">
+                sponsor
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~/assets/scss/common.scss";
+
+.p-sponsor {
+    &__title {
+        @include c-section-title;
+        color: $clr_accentcolor;
+        padding: 0;
+    margin-bottom: 25px;
+    }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@
       <p-hero></p-hero>
       <p-about></p-about>
       <p-access></p-access>
+      <p-sponsor></p-sponsor>
       <L-footer></L-footer>
     </div>
   </main>
@@ -14,6 +15,7 @@
 import PHero from '~/components/top/Hero.vue'
 import PAbout from '~/components/top/About.vue'
 import PAccess from '~/components/top/Access.vue'
+import PSponsor from '~/components/top/Sponsor.vue'
 import Share from "~/components/top/-Share.vue";
 import LFooter from "~/components/Footer.vue";
 
@@ -22,6 +24,7 @@ export default {
     PHero,
     PAbout,
     PAccess,
+    PSponsor,
     LFooter,
     Share
   },
